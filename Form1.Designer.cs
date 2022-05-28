b4094c2e797359393832934577e12897d1199ff7
MaksimMihalcov <maksiswet@gmail.com>
2022-05-28 15:12:05 +0300

    ff

---
+++ Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace RSA
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pval = new System.Windows.Forms.NumericUpDown();
+            this.qval = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.ecpval = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.exp = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pval)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.qval)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ecpval)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.exp)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(604, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(132, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Начать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 26);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(105, 24);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Вычислить";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.Click += new System.EventHandler(this.radioButton1_Click);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 56);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(107, 24);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.Text = "Проверить";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.Click += new System.EventHandler(this.radioButton2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 37);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(173, 93);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Выберите действие";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(128, 26);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pval
+            // 
+            this.pval.Location = new System.Drawing.Point(203, 60);
+            this.pval.Maximum = new decimal(new int[] {
+            -2,
+            2147483647,
+            0,
+            0});
+            this.pval.Name = "pval";
+            this.pval.Size = new System.Drawing.Size(150, 27);
+            this.pval.TabIndex = 5;
+            // 
+            // qval
+            // 
+            this.qval.Location = new System.Drawing.Point(408, 60);
+            this.qval.Maximum = new decimal(new int[] {
+            -2,
+            2147483647,
+            0,
+            0});
+            this.qval.Name = "qval";
+            this.qval.Size = new System.Drawing.Size(150, 27);
+            this.qval.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(203, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "p";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(408, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "q";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 195);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(776, 243);
+            this.textBox1.TabIndex = 9;
+            // 
+            // ecpval
+            // 
+            this.ecpval.Location = new System.Drawing.Point(203, 133);
+            this.ecpval.Maximum = new decimal(new int[] {
+            1000000000,
+            0,
+            0,
+            0});
+            this.ecpval.Name = "ecpval";
+            this.ecpval.Size = new System.Drawing.Size(150, 27);
+            this.ecpval.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(203, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "ecp";
+            // 
+            // exp
+            // 
+            this.exp.Location = new System.Drawing.Point(408, 133);
+            this.exp.Maximum = new decimal(new int[] {
+            1000000000,
+            0,
+            0,
+            0});
+            this.exp.Name = "exp";
+            this.exp.Size = new System.Drawing.Size(150, 27);
+            this.exp.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(408, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 20);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "e";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.exp);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ecpval);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.qval);
+            this.Controls.Add(this.pval);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pval)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.qval)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ecpval)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.exp)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.NumericUpDown pval;
+        private System.Windows.Forms.NumericUpDown qval;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown ecpval;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown exp;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RSA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private class Buf
+        { // Вспомогательный класс для хранения 3-х значений
+            public long d;
+            public long x;
+            public long y;
+            public Buf(long d, long x, long y)
+            {
+                this.d = d;
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        private static Buf GetExtendGcd(long a, long b)
+        {  // Расширенный алгоритм Евклида
+            if (b == 0)
+            {
+                return new Buf(a, 1, 0);
+            }
+            else
+            {
+                Buf tmp = GetExtendGcd(b, a % b);
+                long d = tmp.d;
+                long y = tmp.x - tmp.y * (a / b);
+                long x = tmp.y;
+                return new Buf(d, x, y);
+            }
+        }
+
+        private static long GetHash(string s, long n)
+        {
+            long h = 100;
+            for (int i = 0; i < s.Length; i++)
+            {
+                h = (h + (int)s[i]) * (h + (int)s[i]) % n;
+            }
+            return h;
+        }
+
+        public static bool IsCoprime(long a, long b)
+        {  // Являются ли числа взаимно простыми
+            if (a == b)
+            {
+                return a == 1;
+            }
+            else
+            {
+                if (a > b)
+                {
+                    return IsCoprime(a - b, b);
+                }
+                else
+                {
+                    return IsCoprime(b - a, a);
+                }
+            }
+        }
+
+        private static bool IsPrime(long a)
+        { // Является ли число простым
+            for (long i = 2; i <= Math.Sqrt(a); i++)
+            {
+                if (a % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var r = (long)pval.Value * (long)qval.Value;
+            if (radioButton1.Checked)
+            {
+                var f = ((long)pval.Value - 1) * ((long)qval.Value - 1);
+                if (pval.Value > 1 && qval.Value > 1)
+                {
+                    if (IsPrime((long)pval.Value))
+                    {
+                        if (IsPrime((long)qval.Value))
+                        {
+                            if (((long)exp.Value > 1) && (long)exp.Value < f)
+                            {
+                                if (IsPrime((long)exp.Value) && IsCoprime((long)exp.Value, f))
+                                {
+                                    Buf temp = GetExtendGcd(f, (long)exp.Value);
+                                    long d = temp.y;
+                                    if (d < 0)
+                                    {
+                                        d += f;
+                                    }
+                                    MessageBox.Show($"Ko по алгоритму Евклида: e:{exp.Value} r:{r}");
+                                    MessageBox.Show($"Kc по алгоритму Евклида: d:{d} r:{r}");
+                                    long hash = GetHash(textBox1.Text, r);
+                                    long s = Power(hash, d, r);
+                                    MessageBox.Show($"Хеш: {hash};\nПодпись: {s}");
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Экспонента не взаимно простая с ф.Эйлера!");
+                                }
+                            }
+                            else
+                                MessageBox.Show("Экспонента не вписывается в интервал!");
+                        }
+                        else
+                            MessageBox.Show("Число q не простое!");  
+                    }
+                    else
+                        MessageBox.Show("Число p не простое!");
+                }
+                else
+                    MessageBox.Show("p и q должны быть больше 1!");
+            }
+            else
+            {
+                long newHash = GetHash(textBox1.Text, r);
+                if (newHash == Power((long)ecpval.Value, (long)exp.Value, r))
+                {
+                    MessageBox.Show("Подпись подтверждена");
+                }
+                else
+                {
+                    MessageBox.Show("Подпись недействительна");
+                }
+                MessageBox.Show($"Хеш сообщения: {newHash}\n Хэш по подписи: {Power((long)ecpval.Value, (long)exp.Value, r)}");
+            }
+        }
+
+        private static long Power(long x, long y, long N)
+        {  //Алгоритм быстрого возведения в степень
+            if (y == 0) return 1;
+            long z = Power(x, y / 2, N);
+            if (y % 2 == 0)
+                return (z * z) % N;
+            else
+                return (x * z * z) % N;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //ecpval.Enabled = false;
+        }
+
+        private void radioButton1_Click(object sender, EventArgs e)
+        {
+            //ecpval.Enabled = false;
+            //pval.Enabled = true;
+            //qval.Enabled = true;
+        }
+
+        private void radioButton2_Click(object sender, EventArgs e)
+        {
+            //ecpval.Enabled = true;
+            //pval.Enabled = false;
+            //qval.Enabled = false;
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = File.ReadAllText(openFileDialog1.FileName);
+            }
+        }
+    }
+}
